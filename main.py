e9cd650aa476f7f3c5dfe42d31c5f1d458caa1db
Allen <allenlu@live.ca>
2020-05-02 14:53:59 -0400

    importing twilio packages

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask, request
+from twilio.rest import Client
+from twilio.twiml.messaging_response import Body, Message, Redirect, MessagingResponse
+from twilio import twiml
+
 
 app = Flask(__name__)
 
 @app.route('/send', methods = ['POST'])
 
 
-@app.route('/receive')
+#User receives messages from social media
+@app.route('/receive', methods = ['POST'])

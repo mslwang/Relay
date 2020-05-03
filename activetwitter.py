1d4388c27207e682a951a0905fbbde4bc50f95d9
Michael He <michael.he2001@gmail.com>
2020-05-03 04:26:38 -0700

    now iterating through all users

--- activetwitter.py
+++ activetwitter.py
@@ -11,55 +11,56 @@ from twilio.twiml.messaging_response import Body, Message, Redirect, MessagingRe
 from twilio import twiml
 
 connect(credentials.dbUrl)
-client = MongoClient(credentials.dbUrl).Relay.users
+client = MongoClient(credentials.dbUrl).Relay.user
 
 
 
 #infinite loop baby
 #while True:
-
 active_users = client.find({"active":'twitter'})
 print(active_users)
 
-#for user in active_users:
-    # User API keys (fetched from database)
-consumer_key = credentials.consumer_key
-consumer_secret = credentials.consumer_secret
-access_token_key = credentials.access_token_key
-access_token_secret = credentials.access_token_secret
-lastmsgid = client.twitter_login.last_msg
+for user in active_users:
+        # User API keys (fetched from database)
+    consumer_key = credentials.consumer_key
+    consumer_secret = credentials.consumer_secret
+    access_token_key = credentials.access_token_key
+    access_token_secret = credentials.access_token_secret
+    lastmsgid = client['twitter_login']['last_msg']
 
-api = twitter.Api(
-    consumer_key=consumer_key,
-    consumer_secret=consumer_secret,
-    access_token_key=access_token_key,
-    access_token_secret=access_token_secret)
+    print(lastmsgid)
 
-newmsgid = api.GetDirectMessages(return_json=True, count = 1).events[0].id
+    api = twitter.Api(
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token_key=access_token_key,
+        access_token_secret=access_token_secret)
 
-account_sid = credentials.twil_account_id
-auth_token = credentials.twil_auth_token
-twiloClient = twilClient(account_sid, auth_token)
+    newmsgid = api.GetDirectMessages(return_json=True, count = 1)['events'][0]['id']
 
-if(newmsgid != lastmsgid):
-    #TODO SEND SMS WITH:
-    #This is a JSON with all the messages since the lastmsg
-    newMessages = api.GetDirectMessages(return_json=True, since_id = lastmsgid)
+    account_sid = credentials.twil_account_id
+    auth_token = credentials.twil_auth_token
+    twiloClient = twilClient(account_sid, auth_token)
 
-    recipient = client.find({"consumer_key":'{}'.format(consumer_key)})
-    currentPhoneNum = recipient._id
-    #THIS DOESN'T WORK. Need to get id of the current user
-    sch.User.objects.raw({'_id': currentPhoneNum}).update({"$set": {"twitter_login.last_msg": newmsgid}})
+    if(newmsgid != lastmsgid):
+        #TODO SEND SMS WITH:
+        #This is a JSON with all the messages since the lastmsg
+        newMessages = api.GetDirectMessages(return_json=True, since_id = lastmsgid)
 
-    for msg in newMessages['events']:
-        #Message body
-        actualContent = msg['message_create']['message_data']['text']
-        #Sender name
-        user = api.GetUser(user_id = msg['message_create']['sender_id'])
-        name = user.name
+        recipient = client.find({"consumer_key":'{}'.format(consumer_key)})
+        currentPhoneNum = recipient._id
+        #THIS DOESN'T WORK. Need to get id of the current user
+        sch.User.objects.raw({'_id': currentPhoneNum}).update({"$set": {"twitter_login.last_msg": newmsgid}})
 
-        message = client.messages.create(
-            body=actualContent,
-            from_ = credentials.twil_number,
-            to = recipient
-        )
+        for msg in newMessages['events']:
+            #Message body
+            actualContent = msg['message_create']['message_data']['text']
+            #Sender name
+            user = api.GetUser(user_id = msg['message_create']['sender_id'])
+            name = user.name
+
+            message = client.messages.create(
+                body=actualContent,
+                from_ = credentials.twil_number,
+                to = recipient
+            )

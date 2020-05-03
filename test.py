4e39de11cb575a1971cd522d3f223c4e3a5cfb81
Matthew Wang <mslwang2001@gmail.com>
2020-05-02 21:11:38 -0400

    Make changes to align with other payload formats

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import schema as sch
 
 db = sch.initial()
-sch.User("123", first_name="test", last_name="last", accounts=[sch.Account(platform="MESSENGER", username="test", utype="USERNAME", password="123")]).save()
+sch.User("123", email="test@gmail.com", first_name="test", last_name="last", accounts=[sch.Account(integration="messenger", username="test", utype="username", password="123")]).save()

ef1eae8c966e84c41980928ee58777f0dc709155
Michael He <michael.he2001@gmail.com>
2020-05-03 03:02:10 -0700

    added getting new messages from twitter

--- activetwitter.py
+++ activetwitter.py
@@ -1,17 +1,17 @@
-#every x seconds, scan the list of active twitter users 
+#every x seconds, scan the list of active twitter users
 # and if there are any changes from before, message the user with the new changes
 import json
 import twitter
 from pymongo import MongoClient
 from pymodm import connect, fields, MongoModel, EmbeddedMongoModel
-import credentials 
+import credentials
 
 connect(credentials.dbUrl)
 client = MongoClient(credentials.dbUrl).Relay.users
 
 
 #infinite loop baby
-#while True: 
+#while True:
 
 active_users = client.find({"active":'twitter'})
 print(active_users)
@@ -22,6 +22,7 @@ consumer_key = credentials.consumer_key
 consumer_secret = credentials.consumer_secret
 access_token_key = credentials.access_token_key
 access_token_secret = credentials.access_token_secret
+lastmsgid = client.twitter_login.last_msg
 
 api = twitter.Api(
     consumer_key=consumer_key,
@@ -29,8 +30,16 @@ api = twitter.Api(
     access_token_key=access_token_key,
     access_token_secret=access_token_secret)
 
+newmsgid = api.GetDirectMessages(return_json=True, count = 1).events[0].id
 
+if(newmsgid != lastmsgid):
+    #TODO SEND SMS WITH:
+    #This is a JSON with all the messages since the lastmsg
+    newMessages = api.GetDirectMessages(return_json=True, since_id = lastmsgid)
 
-
-#CODE TO SEE 5 DIRECT MESSAGES. Can be modified to make it since x message or y total messages
-last_message = json.loads(api.GetDirectMessages(return_json=True, count = 1))
+    for msg in newMessages['events']:
+        #Message body
+        actualContent = msg['message_create']['message_data']['text']
+        #Sender name
+        user = api.GetUser(user_id = msg['message_create']['sender_id'])
+        name = user.name

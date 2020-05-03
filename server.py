425b203f2f21807864d9d5081f67bc7c73d96d9b
Kelvin Zhang <kelvin.zhang@uwaterloo.ca>
2020-05-03 04:40:31 +0000

    Update root file

--- server.py
+++ server.py
@@ -1,18 +1,83 @@
 import requests
-from flask import Flask, request, send_from_directory
-import time
 import json
+import time
+import os
+import sys
+from twilio.rest import Client
+from twilio.twiml.messaging_response import Body, Message, Redirect, MessagingResponse
+from twilio import twiml
+from flask import Flask, request, send_from_directory
 from flask_cors import CORS
-import schema as sch
+from fbchat import Client
+from fbchat.models import *
+import getpass
+from pymongo import MongoClient
+from pymodm import connect, fields, MongoModel, EmbeddedMongoModel
+import os
+#from dotenv import load_dotenv
+#import schema as sch
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build')
 CORS(app)
+mongoClient = MongoClient('localhost', 27017)
 
-# sample API that gets time, replace with the following API calls:
-#   * add new user to mongo
-#   * create an endpoint which twilio will call (integrate send.py) 
+client = Client('kelvin.zhang@uwaterloo.ca', getpass.getpass())
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+#user is sending something
+@app.route('/sms', methods = ['POST'])
+def incoming_sms():
+    #Get the message
+    body = request.values.get('Body', None)
+    resp = MessagingResponse()
+
+    cmds = {
+        "getfriends": "",
+        "msg {name} {msg}": "send message to friend",
+        "getactivechats": "print a list of active chats"
+    }
+
+    cmd = body.split(' ', 2)[0].lower()
+
+    if cmd == "getactivechats":
+        users = client.fetchAllUsers()
+        returnMessage = ''
+        for user in users:
+            returnMessage += "{}: {}\n".format(user.name, user.uid)
+
+        resp.message(returnMessage)
+        
+    elif cmd == "msg" or cmd == 'message':
+        recipient = body.split(' ', 2)[1]
+        user = client.searchForUsers(recipient)[0]
+        userId = user.uid
+
+        actualMessage = body.split(' ', 2)[2]
+        targetType = ThreadType.USER
+        
+        client.send(Message(text=actualMessage), thread_id=userId, thread_type=targetType)
+
+        resp.message("Message Sent")
+    
+    else:
+        resp.message("Invalid Command")
+
+    return str(resp)
+
+
 @app.route('/signup', methods = ['POST'])
 def do_signup():
+
     data = json.loads(request.data)
     integration = data['integration']
     tel = data['tel']
@@ -21,9 +86,17 @@ def do_signup():
 
     # store this in mongo
     sch.User(tel, email=email, accounts=[sch.Account(integration=integration, username=email, utype="email", password=password)]).save()
-    print("{}, {}, {}, {}".format(integration, tel, email, password))
+    #print("{}, {}, {}, {}".format(integration, tel, email, password))
     return json.dumps({"status": 200})
 
+@app.route('/exit', methods = ['GET'])
+def exit():
+    client.logout()
+
 if __name__ == "__main__":
-    sch.initial()
     app.run(threaded=True, port=5000)
+
+        
+
+
+    

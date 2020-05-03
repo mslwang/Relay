ae199bf4fdbd92e48f13856710e1b80d6e5104e6
Allen <allenlu@live.ca>
2020-05-03 09:15:14 -0400

    minor clean up

--- retrieve.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-import os
-import pymongo
-
-client = pymongo.MongoClient(os.getenv("CONNECT"))
-
--- test.py
+++
@@ -1,6 +0,0 @@
-import schema as sch
-
-db = sch.initial()
-#sch.User("123", email="test@gmail.com", first_name="test", last_name="last", accounts=[sch.Account(integration="messenger", username="test", utype="username", password="123")]).save()
-#test = sch.User.objects.get({'_id': "123"}).accounts
-a = sch.User.objects.raw({'_id': "123"}).update({"$set": {"active": "twitter"}})
